781b10b7c2c56cf3ec28c227be510275c7e82582
Albania Musabeli <wfav277@gmail.com>
2022-01-16 16:50:46 -0300

    Agregar capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bani.tybani',
+  appName: 'ty-bani',
+  webDir: 'dist/ty-bani',
+  bundledWebRuntime: false
+};
+
+export default config;
