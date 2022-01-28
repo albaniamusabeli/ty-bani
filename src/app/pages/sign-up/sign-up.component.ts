a4e10a6e60ca07056f0277469947c51109c5100d
Albania Musabeli <wfav277@gmail.com>
2022-01-28 19:50:42 -0300

    Agregar redireccion de login a home y se agrega toast

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 import { Usuario } from 'src/app/interfaces/usuario';
 import { LoginService } from 'src/app/services/login.service';
 
@@ -19,7 +21,9 @@ export class SignUpComponent implements OnInit {
   }
 
   constructor(private fb: FormBuilder,
-              private loginService: LoginService) { }
+              private loginService: LoginService,
+              private toastCtrl: ToastController,
+              private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -27,7 +31,7 @@ export class SignUpComponent implements OnInit {
   formularioSignUp = this.fb.group({
     nombre: ['', Validators.required],
     correo: ['', Validators.required],
-    contrasena: ['', Validators.required]
+    contrasena: ['', [Validators.required, Validators.minLength(6)]]
   })
 
   //FUNCION DEL BOTON SIGN UP PARA EL NGSUBMIT
@@ -41,20 +45,40 @@ export class SignUpComponent implements OnInit {
 
     // El usuario es registrado en firebase y nos retorna el uid desde data.user
     this.loginService.registrarUsuario(this.usuarioRegistro).then( (data)=>{
-      console.log('USUARIO REGISTRADO');
+      //console.log('USUARIO REGISTRADO');
+      this.mensajeToast(this.usuarioRegistro.nombre)
       // Cuando el registro es exitoso, se guarda el uid en el objeto usuarioRegistro
       this.usuarioRegistro.uid = data.user.uid
       
       // Con los datos del usuario listos, se guardan en firestore!!
       this.loginService.guardarUsuario(this.usuarioRegistro).then(()=>{
         console.log('USUARIO REGISTRADO EN FIRESTORE');
-        
+
+        //Redireccion a home despues de crear la cuenta (con router)
+        this.router.navigate(['/login'])
       })
       
-      
-
-      
+    }).catch(error=>{
+      this.mensajeErrorToast(error)
     })
   }
 
+  // TOAST para mensaje
+  async mensajeToast(usuario: any) {
+    const toast = await this.toastCtrl.create({
+      message: `Usuario ${usuario} Registrado`,
+      duration: 2000
+    });
+    toast.present();
+  }
+
+
+  // TOAST para mensaje ERROR
+  async mensajeErrorToast(error: any) {
+    const toast = await this.toastCtrl.create({
+      message: error.message,
+      duration: 4000
+    });
+    toast.present();
+  }
 }
