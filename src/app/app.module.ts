506039ea2c01979a31cfd8f38e8efaf83643dce9
Albania Musabeli <wfav277@gmail.com>
2022-01-29 14:56:16 -0300

    Instalacion y configuracion de ion-icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -27,6 +27,9 @@ import { HeaderComponent } from './shared/components/header/header.component';
     LoginComponent,
     SignUpComponent,
     HeaderComponent
+  ],
+  schemas:[
+    CUSTOM_ELEMENTS_SCHEMA
   ],
   imports: [
     BrowserModule,
