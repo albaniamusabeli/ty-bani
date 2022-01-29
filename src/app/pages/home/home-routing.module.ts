6879374a08022527d53fc0d0cb23d0bcfebf31d7
Albania Musabeli <wfav277@gmail.com>
2022-01-29 19:45:42 -0300

    Componentes tabs listos

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgregarTyComponent } from 'src/app/shared/components/agregar-ty/agregar-ty.component';
+import { ListarTyComponent } from 'src/app/shared/components/listar-ty/listar-ty.component';
+import { HomeComponent } from './home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent,
+    children:[
+      {
+        path: 'agregar',
+        component: AgregarTyComponent
+      },
+      {
+        path: 'listar',
+        component: ListarTyComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
